82822ef2c8129e9c87700c48cbf1d2dd2a25916f
Miles Bardon <milesbardon@gmail.com>
2021-06-07 20:39:46 +0100

    Triggering function from pubsub

--- deployment/functions.tf
+++ deployment/functions.tf
@@ -5,8 +5,11 @@ resource "google_cloudfunctions_function" "carto_janitor" {
   available_memory_mb   = 128
   source_archive_bucket = "${var.app_name}-code"
   source_archive_object = google_storage_bucket_object.janitor_archive.name
-  trigger_http          = true
   entry_point           = "trigger"
+  event_trigger {
+    event_type = "google.pubsub.topic.publish"
+    resource   = google_pubsub_topic.janitor.name
+  }
 
   environment_variables = {
     DISCORD_TOKEN = var.discord_token
